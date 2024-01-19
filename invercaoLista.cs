cd3a86da7f07542f00611bb07f8d5aff69b48bf4
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-01-19 17:28:12 -0300

    Update invercaoLista.cs

--- invercaoLista.cs
+++ invercaoLista.cs
@@ -1,4 +1,5 @@
 
+//ATIVIDADE 01
 string pangram = "The quick brown fox jumps over the lazy dog";
 
 string[] menssagem = pangram.Split(" ");
@@ -14,3 +15,20 @@ for(int i = 0; i < menssagem.Length; i++)
 string resultado = String.Join(" ", newMessagem);
 Console.WriteLine(resultado);
 
+
+//ATIVIDADE 02
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] menssagem = orderStream.Split(",");
+Array.Sort(menssagem);
+
+foreach(var menssagens in menssagem)
+{
+    if(menssagem.Length == 4)
+    {
+        Console.WriteLine(menssagem);
+    }
+    else
+    {
+        Console.WriteLine(menssagem + "\t- Erro");
+    }
+}
